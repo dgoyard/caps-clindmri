4c2574792552f67ace1a97f340d0ded36d364bff
David Goyard <david.goyard@cea.fr>
2016-03-09 10:41:51 +0100

    clindmri/plot: adding freesurfer snaps computation using freesurfer_qa tools

---
+++ clindmri/plot/wm_pial_snaps.tcl
@@ -0,0 +1,42 @@
+#DETAILED WM SNAPS
+LoadMainSurface 0 lh.white
+LoadMainSurface 1 rh.white
+
+SetCursor 0 128 128 128
+SetOrientation 0
+SetCursor 0 0 0 0
+
+for { set slice 35 } { $slice <= 215 } { incr slice 1 } {
+
+    SetSlice $slice
+    RedrawScreen
+    SaveRGB $subjdir/QA/$subject/rgb/snaps/snapshot-wm-C-$slice.rgb
+
+}
+
+SetCursor 0 128 128 128
+SetOrientation 1
+SetCursor 0 0 0 0
+
+for { set slice 35 } { $slice <= 215 } { incr slice 1 } {
+
+    SetSlice $slice
+    RedrawScreen
+    SaveRGB $subjdir/QA/$subject/rgb/snaps/snapshot-wm-A-$slice.rgb
+
+}
+
+SetCursor 0 128 128 128
+SetOrientation 2
+SetCursor 0 0 0 0
+
+for { set slice 35 } { $slice <= 215 } { incr slice 1 } {
+
+    SetSlice $slice
+    RedrawScreen
+    SaveRGB $subjdir/QA/$subject/rgb/snaps/snapshot-wm-S-$slice.rgb
+
+}
+
+UnloadSurface 0
+UnloadSurface 1
